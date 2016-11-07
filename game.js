e5891fee9b469c794f8b76657c2f92dc7614da61
alichristensen <ali.christensen1@gmail.com>
2016-11-07 12:52:21 -0600

    tables added to game board

---
+++ game.js
@@ -0,0 +1,21 @@
+var canvas 	= document.getElementById("canvas"), 
+	ctx 	= canvas.getContext("2d");
+
+var tableRad = 35;
+
+function tables_draw(x, y) {
+	ctx.beginPath(); 
+	ctx.arc(x, y, tableRad, 0, Math.PI*2); 
+	ctx.fillStyle = "red"; 
+	ctx.fill();
+	ctx.closePath;
+} 
+
+//fill board with tables: 
+tables_draw(70, 100);
+tables_draw(200, 180);
+tables_draw(300, 260);
+tables_draw(100, 280);
+tables_draw(260, 70);
+tables_draw(388, 170);
+tables_draw(410, 50);
